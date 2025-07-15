16b8831b4150bf38c26d0e486dce5fd0c845981c
hisd3dev3 <rdquilicot14@gmail.com>
2025-07-15 17:00:39 +0800

    secure not hardcoded

--- src/index.ts
+++ src/index.ts
@@ -43,8 +43,7 @@ const server = new ApolloServer({
 			saveUninitialized: false,
 			cookie: {
 				httpOnly: true,
-				// secure: process.env.NODE_ENV === "production" do this when production,
-				secure: false,
+				secure: process.env.NODE_ENV === "production",
 				sameSite: "lax",
 				maxAge: 1000 * 60 * 60 * 24, // 1 day
 			},
